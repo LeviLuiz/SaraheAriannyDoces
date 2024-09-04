d7648c8cc8816e720415e6139c4807d812c74637
Levi Luiz <leviluizmodesto@gmail.com>
2024-09-04 10:56:20 -0300

    Alterando Img

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toogleMode() {
   const html = document.documentElement
-  html.classList.toggle("light")
+  html.classList.toggle("light")  
 
   //se (html.classList.conter("light")) {
   //html.classList.remover("light")
@@ -14,7 +14,7 @@ function toogleMode() {
   //substituir a imagem
   if (html.classList.contains("light")) {
     //se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/sarah.png")
+    img.setAttribute("src", "./assets/sarah-light.png")
   } else {
     //se tiver sem light mode, manter a imagem escura
     img.setAttribute("src", "./assets/sarah.png")
